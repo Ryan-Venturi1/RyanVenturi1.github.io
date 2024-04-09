a7dedd6dcdf9ebc9bf93d52f9efb445243c37579
Ryan-Venturi1 <Ryan.venturi@colorado.edu>
2024-04-09 10:49:58 -0600

    Stuff

---
+++ Ica12/ica12.js
@@ -0,0 +1,50 @@
+addEventListener('load', getQoute);
+
+const btn = document.querySelector('#js-new-quote');
+btn.addEventListener('click', getQoute);
+
+const answerBtn = document.querySelector('#js-tweet');
+AnswerBtn.addEventListener('click', getAnswer);
+
+const answerText = document.querySelector('#js-answer-text');
+
+
+const endpoint= "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+
+let answer = "";
+
+
+async function getQoute() {
+    try {
+        const response = await fetch (endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+        
+        displayQoute(json["question"]);
+        answer=json["answer"];
+        
+
+        
+    }
+    catch(err){
+        console.log(err);
+        alert("Failed to fetch new quote");
+    }
+
+    function displayQoute(qoute){
+        const qouteText = document.querySelector("#js-quote-text");
+        qouteText.textContent = qoute;
+
+    }
+
+    function getAnswer(){
+        console.log(212);
+        answerText.textContent = answer;
+    
+    }
+    
+}
+
